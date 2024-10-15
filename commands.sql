48add6a0299991334ef04e3ccb8e74bd3b0b3556
Bang Solly <bangsolly69@gmail.com>
2024-10-14 21:28:12 -0500

    added queriess in commands.sql

--- commands.sql
+++ commands.sql
@@ -1,16 +1,22 @@
 -- Get the name of the store and its URL where any spices were purchased.
-SELECT DISTINCT s.store_name, st.url_address
-FROM spices_purchased
-JOIN stores st ON s.store_id = st.store_id;
 
 -- Get the name of the store and its URL where spices were purchased, including their barcode.
 
 -- Get stores who only have an online address.
+select store_name
+from store_addresses
+WHERE physical_address IS NULL;
 
 -- Get stores who have an online and a physical address.
+select store_name   
+from store_addresses
+WHERE physical_address and url_address IS NOT NULL;
 
 -- Get spices whose names are also brands. For example, Fresh Direct is a store that also makes spices.
 
--- Get spices whose names contain “cinnamon”.
 
+-- Get spices whose names contain “cinnamon”.
+SELECT spice_name       
+from spices      
+WHERE spice_name LIKE '%cinnamon%';
 -- Get the brand name of the Sumac spice and the name and URL of the store where it was purchased.
